61a270f62c195f12d218926b79f06da5f04a30f7
JackyLiuXiang <904045086@qq.com>
2020-04-26 00:23:30 +0800

    first commit

---
+++ src/com/atguigu/mybatis/dao/EmployeeDao.java
@@ -0,0 +1,7 @@
+package com.atguigu.mybatis.dao;
+
+import com.atguigu.mybatis.beans.Employee;
+
+public interface EmployeeDao {
+    public Employee getEmployeeById(Integer id );
+}
---
+++ src/com/atguigu/mybatis/test/TestMybatis.java
@@ -0,0 +1,57 @@
+package com.atguigu.mybatis.test;
+
+import com.atguigu.mybatis.beans.Employee;
+import com.atguigu.mybatis.dao.EmployeeDao;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TestMybatis {
+    @Test
+    public void testsqlsessionfactory() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+        System.out.println(sqlSessionFactory);
+
+        SqlSession session  = sqlSessionFactory.openSession();
+        try {
+            Employee employee =
+                    session.selectOne("suibian.selectEmployee", 1);
+            System.out.println(employee);
+        } finally {
+            session.close();
+        }
+    }
+
+
+    @Test
+    public void testsqlsessionFactoryMapper() throws IOException {
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+        System.out.println(sqlSessionFactory);
+
+        SqlSession session  = sqlSessionFactory.openSession();
+
+        try {
+            //Mapper接口:获取Mapper接口的 代理实现类对象
+            EmployeeDao dao = session.getMapper(EmployeeDao.class);
+            Employee employee = dao.getEmployeeById(1);
+            System.out.println(employee);
+        } finally {
+            session.close();
+        }
+    }
+
+}
+
+
